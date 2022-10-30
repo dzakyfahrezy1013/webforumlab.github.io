92c32c88a7952b4ec3908b9fe744d410117865b1
dzakyfahrezy1013 <20sa1013@mhs.amikompurwokerto.ac.id>
2022-10-30 11:46:16 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -35,7 +35,7 @@ $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
     App\Console\Kernel::class
 );
-
+();
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
